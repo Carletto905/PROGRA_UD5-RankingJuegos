e45f77b89884b640eac9154fba36632476031c74
Carletto905 <usr2792@salesianos-lacuesta.net>
2025-05-20 12:33:59 +0100

    Update Utilidades.java
    Clase Utilidades actualizado

--- src/util/Utilidades.java
+++ src/util/Utilidades.java
@@ -3,7 +3,7 @@ package util;
 import javax.swing.*;
 
 public class Utilidades {
-    
+
     public static String solicitarTexto(String mensaje) {
         String entrada;
         do {
@@ -18,23 +18,19 @@ public class Utilidades {
             try {
                 puntuacion = Integer.parseInt(JOptionPane.showInputDialog(null, mensaje));
                 if (puntuacion >= 1 && puntuacion <= 5) return puntuacion;
-                else throw new Exception("Rango inválido");
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, "Introduce un número entre 1 y 5.");
-            }
+            } catch (Exception ignored) {}
+            JOptionPane.showMessageDialog(null, "Introduce un número válido entre 1 y 5.");
         }
     }
 
     public static int solicitarIndice(String mensaje, int max) {
-        int indice = -1;
+        int indice;
         while (true) {
             try {
                 indice = Integer.parseInt(JOptionPane.showInputDialog(null, mensaje));
                 if (indice >= 0 && indice < max) return indice;
-                else throw new Exception("Índice fuera de rango");
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, "Introduce un número válido entre 0 y " + (max - 1));
-            }
+            } catch (Exception ignored) {}
+            JOptionPane.showMessageDialog(null, "Introduce un índice válido entre 0 y " + (max - 1));
         }
     }
 }
